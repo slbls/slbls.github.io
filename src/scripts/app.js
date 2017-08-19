d72c66a62e8af70a71c51dc80d4d4f3103c62cd1
Spencer Berenson <spencer@berenson.me>
2017-08-18 23:22:03 -0700

    Starting over with new idea.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,11 @@
 import "../styles/theme.scss";
+
+function overflowStatus(element) {
+	const status = {};
+	if(element.offsetHeight < element.scrollHeight) status.height = true;
+	if(element.offsetWidth < element.scrollWidth) status.width = true;
+
+	return Object.keys(status).length === 0 && status.constructor === Object ? false : status;
+}
+
+// TODO: Determine system for ASCII layout generation.
