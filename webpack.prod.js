ea54a0329009a5f604c484677db2477000706260
Spencer Berenson <spencer@berenson.me>
2018-01-13 08:39:53 -0500

    Initial implementation of mobile design.
    This still requires tweaking for bigger device sizes and larger changes for desktop screens.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,13 @@
 const webpack = require("webpack"),
 merge = require("webpack-merge"),
-common = require("./webpack.common.js"),
-MinifyPlugin = require("babel-minify-webpack-plugin");
+common = require("./webpack.common.js");
 
 module.exports = merge(common, {
 	devtool: "source-map",
 	plugins: [
-		new MinifyPlugin({}, 
-		{
-			comments: false
+		new webpack.optimize.UglifyJsPlugin({
+			comments: false,
+			sourceMap: true
 		})
 	]
 });
