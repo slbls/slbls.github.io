634583a75ceaf2dc318f8fc7da17fa2ea9777788
Spencer Berenson <spencer@berenson.me>
2018-01-19 21:59:58 -0500

    IE < 10 fallback.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,1 @@
 import "../styles/theme.scss";
-
-window.addEventListener("wheel", event => {
-	console.log("yes");
-	const slides =document.getElementsByClassName("slide");
-	slides[0].classList.add("slide--inactive");
-	slides[2].classList.remove("slide--inactive");
-});
