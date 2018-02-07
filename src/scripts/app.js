148a0d62238c8d022ae240e434b16abee0479017
Spencer Berenson <spencer@berenson.me>
2018-02-07 08:43:32 -0500

    Add project section and smooth-scroll polyfill.
    Also replace SCSS placeholder classes with mixins.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,12 @@
 import "../styles/theme.scss";
 
+require("smoothscroll-polyfill").polyfill(); 
+
 console.log("Hey there – welcome to the land of Spencer's code!");
 console.log("Feel free to take a look around. I have done my best to keep it clean and concise for you.");
 console.log("Enjoy!");
+
+document.querySelector(`a[href="#projects"]`).addEventListener("click", function (event) {
+	event.preventDefault();
+	document.getElementById("projects").scrollIntoView({ behavior: "smooth" });
+});
