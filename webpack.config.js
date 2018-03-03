01831474551715259e19aa04e31a015935eeee1a
Spencer Berenson <spencer@berenson.me>
2018-03-03 14:08:37 -0500

    Revert back to Webpack.

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require("path"),
+	CleanPlugin = require("clean-webpack-plugin"),
+	ExtractTextPlugin = require("extract-text-webpack-plugin"),
+	HtmlPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	entry: {
+		"scripts/app": "./src/scripts/app.js"
+	},
+	output: {
+		path: path.join(__dirname, "dist"),
+		filename: "[name]-[hash].js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.html$/,
+				use: "html-loader"
+			},
+			{
+				test: /\.(jpg|jpeg|png|ico|svg)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "[name].[ext]",
+							outputPath: "images/"
+						}
+					}
+				]
+			},
+			{
+				test: /\.(eot|woff|woff2)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "[name].[ext]",
+							outputPath: "fonts/"
+						}
+					}
+				]
+			},
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					use: [
+						{
+							loader: "css-loader",
+							options: { minimize: "true" }
+						},
+						"postcss-loader"
+					],
+					fallback: "style-loader",
+					publicPath: "../"
+				})
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: "babel-loader"
+			}
+		]
+	},
+	plugins: [
+		new CleanPlugin("dist"),
+		new ExtractTextPlugin("styles/[name]-[contenthash].css"),
+		new HtmlPlugin({
+			template: "src/index.html"
+		})
+	]
+};
