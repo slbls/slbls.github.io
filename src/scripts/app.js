af070b9f4915daeb9641b7b59710fcd64f03b045
Spencer Berenson <spencer@berenson.me>
2018-03-29 06:43:45 -0400

    Remove console logs

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,1 @@
 import "../styles/theme.scss";
-
-console.log("Hey there – welcome to the land of Spencer's code!");
-console.log("Feel free to take a look around. I have done my best to keep it clean and concise for you.");
-console.log("Enjoy!");
