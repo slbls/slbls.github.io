34e279f30cd1594a1afc807c6d50e7b473a8c56d
Spencer Berenson <spencer@berenson.me>
2018-04-13 19:38:02 -0400

    Add multiple heading icons
    A different, randomly selected icon will now be shown per page load.
    If JavaScript is disabled, the first of the icons (technologist) will
    show.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,4 @@
 import "../styles/theme.scss";
+
+const headingIcons = document.getElementsByClassName("heading__icon");
+headingIcons[Math.floor(Math.random() * headingIcons.length)].style.display = "block";
