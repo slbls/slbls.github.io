64a49bfc3048b31a7b2d066f30b49c9cfaa2a2e0
Spencer Berenson <spencer@berenson.me>
2018-06-12 13:35:34 -0400

    Adjust whitespace
    Yeah, I admit it—I'm pedantic

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,6 +2,6 @@ import "../styles/theme.scss";
 
 const headingIcons = document.getElementsByClassName("heading__icon"),
 	headingIcon = headingIcons[Math.floor(Math.random() * headingIcons.length)];
-headingIcon.style.display = "block";
 
+headingIcon.style.display = "block";
 document.getElementById("favicon").href = headingIcon.src;
