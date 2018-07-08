5cb501e5ef902b6fa8a6ed593bf48e689b5bebc8
Spencer Berenson <spencer@berenson.me>
2018-07-07 21:59:07 -0400

    Remove previous design's code to create skeleton for new design

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,1 @@
 import "../styles/theme.scss";
-
-const headingIcons = document.getElementsByClassName("heading__icon"),
-	headingIcon = headingIcons[Math.floor(Math.random() * headingIcons.length)];
-
-headingIcon.style.display = "block";
-document.getElementById("favicon").href = headingIcon.src;
