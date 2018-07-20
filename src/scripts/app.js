80978ed03b747fad61c1917009176f6d6e49a4c0
Spencer Berenson <spencer@berenson.me>
2018-07-20 17:39:12 -0400

    Prevent :focus style applying to links on click

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,11 @@
 import "../styles/theme.scss";
+
+let isClick;
+document.body.addEventListener("click", () => (isClick = true));
+document.body.addEventListener("keypress", () => (isClick = false));
+
+document.body.addEventListener("focusout", event => {
+	if (!isClick) return;
+
+	setTimeout(() => event.target.blur(), 0);
+});
