59974eab05daeb334b0c11946e50f03977fe551e
Spencer Berenson <spencer@berenson.me>
2018-08-07 14:10:33 -0400

    Remove PurifyCSS (it proved to be error-prone and not very accurate)

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,21 +1,8 @@
 const webpack = require("webpack"),
 	merge = require("webpack-merge"),
-	commonConfiguration = require("./webpack.common"),
-	path = require("path"),
-	globby = require("globby"),
-	PurifyCssPlugin = require("purifycss-webpack");
+	commonConfiguration = require("./webpack.common");
 
 module.exports = merge(commonConfiguration, {
 	mode: "production",
-	devtool: "source-map",
-	plugins: [
-		new PurifyCssPlugin({
-			paths: globby.sync(path.join(__dirname, "src/pug/*.pug")),
-			minimize: true,
-			purifyOptions: {
-				info: true,
-				rejected: true
-			}
-		})
-	]
+	devtool: "source-map"
 });
