d3032f1c0c429fda2db4d94281b16e325f46615c
Spencer Berenson <spencer@berenson.me>
2018-08-07 13:39:06 -0400

    Add CSS purification with PurifyCSS

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,21 @@
 const webpack = require("webpack"),
 	merge = require("webpack-merge"),
-	commonConfiguration = require("./webpack.common");
+	commonConfiguration = require("./webpack.common"),
+	path = require("path"),
+	globby = require("globby"),
+	PurifyCssPlugin = require("purifycss-webpack");
 
 module.exports = merge(commonConfiguration, {
 	mode: "production",
-	devtool: "source-map"
+	devtool: "source-map",
+	plugins: [
+		new PurifyCssPlugin({
+			paths: globby.sync(path.join(__dirname, "src/pug/*.pug")),
+			minimize: true,
+			purifyOptions: {
+				info: true,
+				rejected: true
+			}
+		})
+	]
 });
