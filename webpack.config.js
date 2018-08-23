2fc9e71baf204f1d7666e06a6c2810dd4a8557ff
Spencer Berenson <spencer@berenson.me>
2018-08-23 15:49:51 -0400

    Consolidate Webpack configurations into one file

---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+const path = require("path"),
+	CleanPlugin = require("clean-webpack-plugin"),
+	HtmlPlugin = require("html-webpack-plugin"),
+	ScriptExtHtmlPlugin = require("script-ext-html-webpack-plugin"),
+	MiniCssExtractPlugin = require("mini-css-extract-plugin"),
+	WebappPlugin = require("webapp-webpack-plugin"),
+	CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = (_env, options) => {
+	const isProduction = options.mode.toLowerCase() === "production",
+		plugins = [
+			new MiniCssExtractPlugin({
+				filename: "static/css/bundle-[hash].css"
+			}),
+			new HtmlPlugin({
+				template: "src/pug/index.pug"
+			}),
+			new ScriptExtHtmlPlugin({
+				defaultAttribute: "defer"
+			}),
+			new WebappPlugin({
+				logo: "./src/favicon.png",
+				prefix: "icons/",
+				inject: true,
+				favicons: {
+					background: "#f5f5f5",
+					theme_color: "#333333",
+					display: "browser",
+					icons: {
+						android: true,
+						appleIcon: true,
+						appleStartup: false,
+						coast: false,
+						favicons: true,
+						firefox: true,
+						windows: true,
+						yandex: true
+					}
+				}
+			})
+		];
+
+	if (isProduction) {
+		plugins.push(new CleanPlugin("dist"));
+		plugins.push(
+			new CopyPlugin([
+				{ from: "_redirects", to: "_redirects", toType: "file" }
+			])
+		);
+	}
+
+	return {
+		devtool: isProduction ? "source-map" : "inline-source-map",
+		entry: path.resolve("src/scripts/index.js"),
+		output: {
+			path: path.resolve("dist"),
+			filename: "static/js/bundle-[hash].js"
+		},
+		resolve: {
+			extensions: [".js", ".json", ".jsx"]
+		},
+		module: {
+			rules: [
+				{
+					test: /.pug$/,
+					use: [
+						{
+							loader: "html-loader",
+							options: {
+								minimize: true
+							}
+						},
+						"pug-html-loader"
+					]
+				},
+				{
+					test: /.(c|sc|sa)ss$/,
+					use: [
+						isProduction
+							? MiniCssExtractPlugin.loader
+							: "style-loader",
+						{
+							loader: "css-loader",
+							options: { minimize: "true" }
+						},
+						"postcss-loader",
+						"sass-loader"
+					]
+				},
+				{
+					test: /\.j(s|sx)$/,
+					exclude: /node_modules/,
+					use: ["babel-loader"]
+				}
+			]
+		},
+		plugins: plugins
+	};
+};
--- webpack.dev.js
+++
@@ -1,8 +0,0 @@
-const webpack = require("webpack"),
-	merge = require("webpack-merge"),
-	commonConfiguration = require("./webpack.common");
-
-module.exports = merge(commonConfiguration, {
-	mode: "development",
-	devtool: "inline-source-map"
-});
