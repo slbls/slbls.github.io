7d0deec5569f6e26c2e81a6cee25bb38abe9811f
Spencer Berenson <spencer@berenson.me>
2018-09-17 17:42:08 -0400

    Rename index.ts back to index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import "normalize.css/normalize.css";
+import "sanitize.css";
 import "./index.scss";
 
-let isClick: boolean;
+let isClick;
 document.body.addEventListener("click", () => (isClick = true));
 document.body.addEventListener("keypress", () => (isClick = false));
 
 document.body.addEventListener("focusout", event => {
 	if (!isClick) return;
 
-	const target = event.target as HTMLElement;
+	const target = event.target;
 	setTimeout(() => target.blur(), 0);
 });
