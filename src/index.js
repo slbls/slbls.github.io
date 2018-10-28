a29d2e8453a75c9b9cd076db84c61960d4d96d0c
Spencer Berenson <spencer@berenson.me>
2018-10-27 21:28:18 -0400

    Remove site content and styles in preparation for new design

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
 import "sanitize.css";
 import "./index.scss";
-
-let isClick;
-document.body.addEventListener("click", () => (isClick = true));
-document.body.addEventListener("keypress", () => (isClick = false));
-
-document.body.addEventListener("focusout", event => {
-	if (!isClick) return;
-
-	const target = event.target;
-	setTimeout(() => target.blur(), 0);
-});
