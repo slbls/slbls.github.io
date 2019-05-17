bd9c41eb456b66ea9b34ffc8a6dc1f4023813c52
Spencer Berenson <spencer@berenson.me>
2019-05-17 01:32:55 -0400

    Remove unused JavaScript from previous design

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
 import "normalize.css/normalize.css";
 import "./index.scss";
-
-let isClick;
-document.body.addEventListener("click", () => (isClick = true));
-document.body.addEventListener("keypress", () => (isClick = false));
-
-document.body.addEventListener("focusout", event => {
-	if (!isClick) return;
-
-	const target = event.target;
-	setTimeout(() => target.blur(), 0);
-});
