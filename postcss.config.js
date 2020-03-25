06f903d18a2ee961713fd242d16e8a6ea0429149
Spencer Berenson <spencer@berenson.me>
2020-03-25 10:47:42 -0400

    Introduce postcss-calc and postcss-nested

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 module.exports = {
 	plugins: [
 		require("postcss-easy-import")({ prefix: "_" }),
+		require("postcss-calc"),
 		require("postcss-custom-properties")(),
+		require("postcss-nested"),
 		require("autoprefixer")({ grid: "autoplace" }),
 		require("cssnano")({
 			preset: [
