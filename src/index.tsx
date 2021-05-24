912b3bd81a597ca028345b348aca458efe19f280
Spencer Berenson <spencer@berenson.dev>
2021-05-24 13:45:15 -0400

    Configure react-snap for pre-rendering

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
-);
+const rootComponent = (
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	),
+	rootElement = document.getElementById("root");
+if (rootElement?.hasChildNodes()) {
+	ReactDOM.hydrate(rootComponent, rootElement);
+} else {
+	ReactDOM.render(rootComponent, rootElement);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
