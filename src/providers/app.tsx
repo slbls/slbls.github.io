e292dd78de18cff54a8c8280b4eca9c7cb5f1a17
Spencer Berenson <spencer@berenson.dev>
2021-12-08 12:05:11 -0500

    Create AppMetadataContext for app metadata

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -1,10 +1,44 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { ReactNode } from "react";
+import { createContext, ReactNode, useContext } from "react";
+
+type AppMetadata = {
+	readonly name: string;
+	readonly titleSeparator: string;
+};
+
+const AppMetadataContext = createContext<AppMetadata | undefined>(undefined);
+
+export const useAppMetadataContext = () => {
+	const context = useContext(AppMetadataContext);
+	if (context === undefined) {
+		throw new Error(
+			"useAppMetadataContext must be used within a AppMetadataProvider."
+		);
+	}
+
+	return context;
+};
+
+type AppMetadataProviderProps = {
+	readonly children: ReactNode;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const name = process.env.NEXT_PUBLIC_PACKAGE_NAME!;
+const titleSeparator = " | ";
+
+const AppMetadataProvider = ({ children }: AppMetadataProviderProps) => (
+	<AppMetadataContext.Provider value={{ name, titleSeparator }}>
+		{children}
+	</AppMetadataContext.Provider>
+);
 
 type AppProviderProps = {
 	readonly children: ReactNode;
 };
 
 export const AppProvider = ({ children }: AppProviderProps) => (
-	<ChakraProvider>{children}</ChakraProvider>
+	<ChakraProvider>
+		<AppMetadataProvider>{children}</AppMetadataProvider>
+	</ChakraProvider>
 );
