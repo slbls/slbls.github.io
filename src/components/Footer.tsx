35345470fde78dbed4c4b0c1b13a1e473e6e8bca
Spencer Berenson <spencer@berenson.dev>
2021-12-08 12:17:55 -0500

    Use `<time>` element for the year in Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,20 +5,24 @@ type FooterProps = {
 	readonly email: string;
 };
 
-export const Footer = ({ github, email }: FooterProps) => (
-	<Box as="footer" fontSize="sm">
-		For more,{" "}
-		<Link href={`//github.com/${github}`} isExternal color="purple.800">
-			visit my GitHub profile
-		</Link>{" "}
-		or{" "}
-		<Link href={`mailto:${email}`} color="purple.800">
-			send me an email
-		</Link>
-		.
-		<Box as="br" marginBottom={2} />
-		<Text as="small" fontSize="sm" color="gray.400">
-			© {new Date().getFullYear()}
-		</Text>
-	</Box>
-);
+export const Footer = ({ github, email }: FooterProps) => {
+	const year = new Date().getFullYear();
+
+	return (
+		<Box as="footer" fontSize="sm">
+			For more,{" "}
+			<Link href={`//github.com/${github}`} isExternal color="purple.800">
+				visit my GitHub profile
+			</Link>{" "}
+			or{" "}
+			<Link href={`mailto:${email}`} color="purple.800">
+				send me an email
+			</Link>
+			.
+			<Box as="br" marginBottom={2} />
+			<Text as="small" fontSize="sm" color="gray.400">
+				© <time dateTime={year.toString()}>{year}</time>
+			</Text>
+		</Box>
+	);
+};
