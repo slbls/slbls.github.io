5a510a5e8b51e09f9492c42bb58771fb31a0f1a3
Spencer Berenson <spencer@berenson.dev>
2022-05-26 22:01:27 -0400

    refactor: use .js for ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,12 @@
-{
-	"extends": [
+// @ts-check
+
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+	extends: [
 		"eslint:recommended",
 		"next/core-web-vitals",
 		"plugin:@typescript-eslint/recommended",
 		"plugin:playwright/playwright-test",
-		"plugin:prettier/recommended"
-	]
-}
+		"plugin:prettier/recommended",
+	],
+};
