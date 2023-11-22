632a21b295c639c5ddf8c703d9f128ed64b37bb4
Spencer Berenson <spencer@berenson.dev>
2023-11-22 15:50:14 -0500

    feat(postcss): include Next.js defaults in config

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,18 @@
 module.exports = {
 	plugins: {
+		// Default Next.js configuration
+		"postcss-flexbugs-fixes": {},
+		"postcss-preset-env": {
+			autoprefixer: {
+				flexbox: "no-2009",
+			},
+			stage: 3,
+			features: {
+				"custom-properties": false,
+			},
+		},
+
+		// Tailwind CSS configuration
 		tailwindcss: {},
 		autoprefixer: {},
 		...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {}),
