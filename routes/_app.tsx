ad77e40ab795838483da012cfa48861ab3d261f8
Spencer Berenson <spencer@berenson.dev>
2023-12-20 15:36:15 -0500

    Add lang attribute and viewport tag to head

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,9 +1,10 @@
 import { type PageProps } from "$fresh/server.ts";
 
 const App = ({ Component }: PageProps) => (
-  <html>
+  <html lang="en">
     <head>
       <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="shortcut icon" href="/favicon.svg" type="image/svg" />
       <title>berenson.dev</title>
       <meta name="description" content="Spencer Berenson's personal website." />
