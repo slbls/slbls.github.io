b849c1e568552662896c3abd58ad336d0ed4db53
Spencer Berenson <spencer@berenson.dev>
2024-02-28 19:10:13 -0500

    Migrate Snowflakes poem from external host

--- astro.config.ts
+++ astro.config.ts
@@ -5,5 +5,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://berenson.dev",
-	integrations: [tailwind()],
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+	],
 });
