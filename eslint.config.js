a73705fad3c84a5140ba0eb78bb9ce7bf2abf1a0
Spencer Berenson <spencer@berenson.dev>
2024-03-05 11:57:55 -0500

    Add linting workflow

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import eslint from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+
+export default [
+	eslint.configs.recommended,
+	eslintPluginPrettierRecommended,
+	{
+		plugins: {
+			"simple-import-sort": simpleImportSort,
+		},
+		rules: {
+			"simple-import-sort/imports": "error",
+			"simple-import-sort/exports": "error",
+		},
+	},
+];
